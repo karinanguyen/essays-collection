f4e41d985baf72aadcdf71c74733a7d71cf721ee
Karina Nguyen <karinanguyen@berkeley.com>
2020-10-30 15:45:50 -0700

    first

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>interface sémaphore</h1>
+    <p>this is the collection of essays by Karina Nguyen</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+  </Layout>
+)
+
+export default IndexPage
